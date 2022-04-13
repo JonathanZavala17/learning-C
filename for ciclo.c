fa81ca8f6536bb43dd1bb30234308665fb09a141
JonathanZavala17 <jonathanrivas1197@gmail.com>
2022-04-12 23:07:17 -0600

    V1.2

---
+++ for ciclo.c
@@ -0,0 +1,26 @@
+//ciclo for 
+//mostrar de 1 a 10, ascendente 
+
+#include<stdio.h>
+
+int main(){
+	int i,j;
+	
+	//aumento
+	for(i=1;i<=10;i++){
+		printf("%i ",i);
+	}
+
+	//decremento
+	for(i=10;i>=1;i--){
+		printf("%i ",i);
+	}
+	
+	//planas
+	for(j=1;j<=45;j++){
+		printf("\n %i. No debo saltarme las clases de C",j);
+	}
+	
+	
+	return 0;
+}
